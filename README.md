dbeb2a0a6a706cea0a9c5f0d344f28542bbc2480
NahushaG <nahusha.ganiga@gmail.com>
2026-01-14 16:49:00 +0100

    Initial Commit: Spring Boot project setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Device Management Service
+
+A Spring Boot application for managing devices.
+
+## Tech Stack
+- Java 21
+- Spring Boot 3.5.9
+- Maven
+
+## Build & Run
+```bash
+./mvnw clean spring-boot:run
+
---
+++ src/test/java/org/assessment/devicemanagement/DeviceManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.assessment.devicemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
